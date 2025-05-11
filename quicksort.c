2341fd4075a9b7da608c9de3f0fdecfbf97a35f6
Archana A <145422353+archanaprajith@users.noreply.github.com>
2025-05-12 01:23:26 +0530

    Add files via upload

---
+++ quicksort.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int a[100];
+
+void swap(int low,int j){
+    int temp=a[low];
+    a[low]=a[j];
+    a[j]=temp;
+}
+
+int partition(int low,int high){
+    int pivot;
+    int i,j;
+    pivot=a[low];
+    i=low;
+    j=high;
+    while (i<j){
+        do{
+            i++;
+        }while (i<=high&&a[i]<=pivot);
+        do{
+            j--;
+        }while (j>=low&&a[j]>pivot);
+        if(i<j)
+        swap(i,j);
+        
+    }
+    swap(low,j);
+    return j;   
+}
+
+void quicksort(int low,int high){
+    if(low<high){
+        int j=partition(low,high);
+        quicksort(low,j);
+        quicksort(j+1,high);
+    }
+}
+
+
+
+void main(){
+   int num;
+   printf("Enter length of array\n");
+   scanf("%d",&num);
+   printf("Enter elements \n");
+   for (int i = 0; i < num; i++)
+   {
+    scanf("%d",&a[i]);
+   }
+   a[num]=-1;
+   quicksort(0,num);
+   printf("Sorted elements \n");
+   for (int i = 0; i < num; i++)
+   {
+    printf("%d ",a[i]);
+}
+}
